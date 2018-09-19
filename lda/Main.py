62d35aba6e095a5d98623c849313bb1cc7a7f976
niceinfuture <1065436778@qq.com>
2018-09-19 23:46:15 +0800

    add lda

---
+++ lda/Main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+import numpy as np
+import pandas as pd
+import re
+
+df = pd.read_csv("./input/HillaryEmails.csv")
+
+# print (df.iloc[1:10].pop)
+# 原邮件数据中有很多Nan的值，直接扔了。
+# df = df[['Id','ExtractedBodyText']]
+# .dropna()
+# print (df)
+# print (df.dropna())
