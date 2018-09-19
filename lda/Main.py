601274339cb7b64993de49fb0da6d6a4c4950f1b
niceinfuture <1065436778@qq.com>
2018-09-20 00:25:19 +0800

    test

--- lda/Main.py
+++ lda/Main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import re
 
 df = pd.read_csv("./input/HillaryEmails.csv")
-
+print (df)
 # print (df.iloc[1:10].pop)
 # 原邮件数据中有很多Nan的值，直接扔了。
 # df = df[['Id','ExtractedBodyText']]
